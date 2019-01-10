69a63b7e55fb44a53f13baa9efed42991a468c24
Jude Hudson <jude@4tgogweb.com>
2019-01-10 21:44:58 +0000

    Image compression, css minification, amends to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+// image compression
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
+// File Paths
+var DEST_PATH = 'app';
+var IMAGES_PATH = 'app/src-images/**/*.{png,jpeg,jpg,svg,gif}';
+
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -16,9 +26,28 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
-        .pipe(sass())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
+
+// Images - compress images - run 'gulp image'
+gulp.task('images', function () {
+	return gulp.src(IMAGES_PATH)
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle(),
+                imagemin.jpegtran(),
+                imagemin.optipng(),
+                imagemin.svgo(),
+                imageminPngquant(),
+                imageminJpegRecompress()
+            ]
+        ))
+        .pipe(gulp.dest(DEST_PATH + '/img'));
+});
+
 
 gulp.task('default', ['serve']);
